1be92c8d78107ca97db7e58c3fcc2af7dcb4d40d
seoyeonwkn <dohaloveng@gmail.com>
2025-08-20 12:57:06 +0900

    수정

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,21 +11,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool, default=False)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#+0cp9y@kr6agfm&+q*go%c6=r6%8!qmz03y+rlf0^7fbf+506'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 DEMO_USER_ID = 1
 
@@ -81,11 +75,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
   "default": {
     "ENGINE": "django.db.backends.mysql",
-    "NAME": "hwapizza",
-    "USER": "root",
-    "PASSWORD": "0414",
-    "HOST": "127.0.0.1",
-    "PORT": "3306",
+    "NAME": config("DB_NAME"),
+    "USER": config("DB_USER"),
+    "PASSWORD": config("DB_PASSWORD"),
+    "HOST": config("DB_HOST", default="127.0.0.1"),
+    "PORT": config("DB_PORT", default="3306"),
     "OPTIONS": {"charset": "utf8mb4"},
   }
 }
